f1d0202599ae0faefd9c4199efff458600fa5501
alessandro <alessandroDev@outlook.it>
2026-02-05 16:11:20 +0100

    feat: practice with middlewares

--- app.js
+++ app.js
@@ -5,11 +5,23 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log('Hello from the middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
 );
 
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
+
   res.status(200).json({
     status: 'success',
     results: tours.length,
@@ -87,6 +99,14 @@ const deleteTour = (req, res) => {
 };
 
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
+
+// because the following middleware has been defined after the route handler ('/api/v1/tours')
+// all of the requests regarding the tours, will SKIP the middleware. Order definition is important
+app.use((req, res, next) => {
+  console.log('Hello from the skipped middleware');
+  next();
+});
+
 app
   .route('/api/v1/tours/:id')
   .get(getTourById)
