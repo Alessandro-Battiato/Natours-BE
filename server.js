1f1c9440773cf7c7a413e9fc8d365a54b5afe63a
alessandro <alessandroDev@outlook.it>
2026-02-14 17:05:01 +0100

    feat: define tourSchema and Tour model

--- server.js
+++ server.js
@@ -16,6 +16,24 @@ mongoose
   })
   .then(() => {});
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'], // validator, this validates our data, we have set it here to be required and we also have set an error string
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
