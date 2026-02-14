1991eaab24c333f5923a9269b867beb4be3694b6
alessandro <alessandroDev@outlook.it>
2026-02-14 19:56:05 +0100

    feat: implement filtering for GET tours request

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -27,7 +27,20 @@ exports.checkBody = (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    const query = Tour.find(queryObj);
+
+    /*const query = await Tour.find()
+      .where('duration')
+      .equals(5)
+      .where('difficulty')
+      .equals('easy');*/
+
+    const tours = await query;
 
     res.status(200).json({
       status: 'success',
