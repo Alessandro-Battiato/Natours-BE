5f049388e30629482bc26e7ff3633ff5870cc138
alessandro <alessandroDev@outlook.it>
2026-02-15 17:57:14 +0100

    feat: add slugify and use pre middleware to create a slug prop on newly created documents

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -66,6 +68,24 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7; // we calculate the duration in weeks this way, that is why we are not using an arrow function because we need 'this'
 }); // in this case, the get is called getter and each time we get something for the db, the virtual property we have set will be created and will be returned in the results even if not present in the db
+
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true }); // define a new slug prop on the newly created document
+  next(); // similar to the next middlewares of express, we need to invoke this here
+}); // pre is a middleware that runs before an event, the event is the one specified as the argument of the function so in this case, the function callback we are passing will be called BEFORE we SAVE something in the db
+
+/*
+tourSchema.pre('save', function (next) {
+  console.log('Will save document...');
+  next();
+});
+
+tourSchema.post('save', function (doc, next) {
+  // the doc parameter is the newly created document
+
+  next();
+});
+*/
 
 const Tour = mongoose.model('Tour', tourSchema);
 
