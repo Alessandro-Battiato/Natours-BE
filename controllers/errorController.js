4e2886faa7e4c6a240abf0a18a29f8418d834924
alessandro <alessandroDev@outlook.it>
2026-02-22 19:10:57 +0100

    fix: distinguish between operational and programming errors and app environment as to leak as little information as possible when production errors occur

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,13 +1,40 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to the client, otherwise don't leak anything for programming errors
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error('ERROR', err);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   // By specifying 4 arguments (and in this case the first one becomes the error)
   // express AUTOMATICALLY recognizes this middleware as an error middleware and treats it as one
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 
   // key note: whenever we pass ANYTHING into the next() function, express assumes it is an error and will send the error to the global error handling middleware, skipping all the other middlewares
 };
