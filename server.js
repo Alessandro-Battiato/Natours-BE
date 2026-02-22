030cd6902ccc232a308ad95710bed59001e0e183
alessandro <alessandroDev@outlook.it>
2026-02-22 20:00:23 +0100

    fix: handle uncaught exception errors

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+// Handle ALL uncaught exceptions here, lift this event listener here at the top for uncaught exceptions
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1); // no asynchronous behavior involved here so we can abruptly close the server
+});
+
 dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE.replace(
@@ -26,8 +33,8 @@ const server = app.listen(port, () => {
 
 // Handle ALL promise rejections here
 process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! Shutting down...');
   console.log(err.name, err.message);
-  console.log('UNHANDLER REJECTION! Shutting down...');
   server.close(() => {
     // server close gives time to the server to close "gracefully" because doing only process.exit(1) closes the server abruptly, without caring for pending requests for instance
     process.exit(1);
