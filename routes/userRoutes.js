9bdcdd8c7ab35265a59578ef98bb7a6cb0045d71
alessandro <alessandroDev@outlook.it>
2026-03-01 19:12:34 +0100

    feat: create auth controller and define signup enpdoint

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const userController = require('./../controllers/userController');
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
+// The following DOES NOT follow the RESTful principle regarding names because the signup rout HAS to do with the performed action but it's fine in this scenario
+router.post('/signup', authController.signup);
+
+// The following DO follow the RESTful principle of rout names not having anything to do with the performed action
 router
   .route('/')
   .get(userController.getAllUsers)
