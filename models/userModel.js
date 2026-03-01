5733d43d0af63085abb48c505809e7a6b67cfb89
alessandro <alessandroDev@outlook.it>
2026-03-01 19:27:20 +0100

    feat: implement password validation and encryption using bcrypt to hash passwords

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -22,7 +23,26 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'You must confirm your password'],
+    validate: {
+      // This validation only works on User.create and User.save
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!',
+    },
   },
+});
+
+// Between getting the data, and saving it to the database, we are running the following middleware
+userSchema.pre('save', async function (next) {
+  // this = current document, or rather the current User
+  if (!this.isModified('password')) return next(); // If password has NOT been modified, run next middleware
+
+  // Encrypt (or hash) password using bcrypt to protect from brute force attacks
+  this.password = await bcrypt.hash(this.password, 12); // second argument is salt, random string added to our password, the higher this argument is, the more CPU intensive this operation becomes
+
+  this.passwordConfirm = undefined; // of course the hashed password will not be equal to the old user's password so we no longer need this validation field
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
