b77d85642f5aafaab90e5084bc16d2400e7ce261
alessandro <alessandroDev@outlook.it>
2026-03-14 17:44:50 +0100

    feat: send JWT via cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,6 +19,18 @@ const signToken = (id) =>
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+    ),
+    httpOnly: true, // Prevents Cross-Site Scripting attacks
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
+  user.password = undefined; // so as not to include the password in the output after creating a user
 
   res.status(statusCode).json({
     status: 'success',
