dcf3e0f4e1c2416d7174f88df526beb40b9ddf88
alessandro <alessandroDev@outlook.it>
2026-03-14 17:48:31 +0100

    feat: implement rate limite for requests for security purposes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -12,6 +13,15 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  // how many requests per IP we allow during a time window
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour',
+});
+
+app.use('/api', limiter); // apply this limiter only to /api routes
 
 app.use(express.json());
 
