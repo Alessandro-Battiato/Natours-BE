df1d5d28fa8d5bf697fb441bf3f11418b9197599
alessandro <alessandroDev@outlook.it>
2026-03-14 17:56:34 +0100

    feat: implement data sanitization for NoSQL query injections and XSS attacks

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -28,6 +30,12 @@ const limiter = rateLimit({
 app.use('/api', limiter); // apply this limiter only to /api routes
 
 app.use(express.json({ limit: '10kb' })); // req.bodies larger than 10kb will not be accepted
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
 
 app.use(express.static(`${__dirname}/public`)); // to be able to serve static files
 
